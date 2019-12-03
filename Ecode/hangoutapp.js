f2633f910fcd182f25f6514d0e59b26c3d0511f0
Eric Curlett <curlettt@gmail.com>
2019-12-02 18:07:19 -0800

    Posts to DB and Create user
    my implementation or something

---
+++ Ecode/hangoutapp.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+
+const dotenv = require("dotenv").config();
+
+mongoose.connect("mongodb+srv://Ructell:thisisapassword@hangout-kw3ge.mongodb.net/test?retryWrites=true&w=majority",
+    {useNewUrlParser: true}
+    ).then(() => console.log('DB Connection'));
+mongoose.connection.on('error', err => {
+    console.log(`DB connection error: ${err.message}`);
+});
+// mongoose.connect(process.env.MONGO_URI,
+//     {useNewUrlParser: true}
+//     ).then(() => console.log('DB Connection'));
+// mongoose.connection.on('error', err => {
+//     console.log(`DB connection error: ${err.message}`);
+// });
+
+const postRoutes = require('./routes/post');
+const authRoutes = require('./routes/auth');
+app.use(bodyParser.json());
+app.use(expressValidator());
+app.use("/", postRoutes);
+app.use("/", authRoutes);
+const port = process.env.PORT || 1000;
+app.listen(port, () => {console.log(`API listening on port: ${port}`);
+});
