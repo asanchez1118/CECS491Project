1b0192518f3d89b71b7aab5910ae994b6ccb1c66
asanchez1118 <sanchez.antonio.877@gmail.com>
2020-02-06 02:00:33 -0800

    Clearing clutter

--- Ecode/hangoutapp.js
+++ Ecode/hangoutapp.js
@@ -7,9 +7,12 @@ const expressValidator = require('express-validator');
 
 const dotenv = require("dotenv").config();
 
-mongoose.connect("mongodb+srv://Ructell:thisisapassword@hangout-kw3ge.mongodb.net/test?retryWrites=true&w=majority",
-    {useNewUrlParser: true}
-    ).then(() => console.log('DB Connection'));
+mongoose
+   .connect(process.env.MONGO_URI,{
+      useNewUrlParser: true
+   })
+   .then(() => console.log('DB Connection'));
+
 mongoose.connection.on('error', err => {
     console.log(`DB connection error: ${err.message}`);
 });
@@ -22,10 +25,31 @@ mongoose.connection.on('error', err => {
 
 const postRoutes = require('./routes/post');
 const authRoutes = require('./routes/auth');
+//const userRoutes = require('./routes/user');
+
+app.get("/api", (req, res) => {
+   fs.readFile("docs/apiDocs.json", (err, data) => {
+      if (err) {
+         res.status(400).json({
+            error: err
+         });
+      }
+      const docs = JSON.parse(data);
+      res.json(docs);
+   });
+});
+
 app.use(bodyParser.json());
 app.use(expressValidator());
-app.use("/", postRoutes);
-app.use("/", authRoutes);
-const port = process.env.PORT || 5000;
-app.listen(port, () => {console.log(`API listening on port: ${port}`);
+app.use("/api", postRoutes);
+app.use("/api", authRoutes);
+//app.use("/api", userRoutes);
+app.use(function(err, req, res, next) {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({ error: 'Unauthorized!' });
+    }
+});
+
+const port = process.env.PORT || 80;
+app.listen(port, () => {console.log(`Node.js API listening on port: ${port}`);
 });
