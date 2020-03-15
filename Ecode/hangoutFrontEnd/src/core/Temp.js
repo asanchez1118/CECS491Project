29195925740c26080d49d8a7e7a11b08546974c5
cookiespace <44853070+cookiespace@users.noreply.github.com>
2020-03-14 19:28:32 -0700

    Temp

---
+++ Ecode/hangoutFrontEnd/src/core/Temp.js
@@ -0,0 +1,9 @@
+const a = 'desqwxu7vqhy';
+const b = '65028';
+const c = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoidXNlci1vbmUifQ.1HfktmJgDrnnV7QGJI23mUEszdwtSxcP4ispggsuPqs';
+
+module.exports = {
+  a: a,
+  b: b,
+  c: c
+}
