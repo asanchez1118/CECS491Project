5d5884641dde76fdb2b0417cd7759468ce698400
Dean Vo <dnvo@users.noreply.github.com>
2020-03-16 22:34:10 -0700

    Fixed home page image
    Made it so that the home page image changes size based on the window

--- Ecode/hangoutFrontEnd/src/core/Home.js
+++ Ecode/hangoutFrontEnd/src/core/Home.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import csulb from '../assets/csulb.jpg'
 
 const Home = () => (
-  <img src={csulb} alt="CSULB" width="1918px" resizeMode="contain"/>
+  // <div style="position:fixed; height: 100%; width: 100%; top:0;left 0;">
+  //   <img src='whatever.png' style="height: 100%" />
+  // </div>
+  //<img src={csulb} alt="CSULB" width="1900px" resizeMode={'contain'} alignSelf='flex-start'/>
+  <div>
+    <img src={csulb} alt="CSULB" width="100%"/>
+  </div>
 
   // <div>
   //   <h2>Home</h2>
