5785e284f2ba24608ea501cec3e5c853d2a66c03
cookiespace <44853070+cookiespace@users.noreply.github.com>
2020-05-01 15:51:58 -0700

    Indiv Source (B)

---
+++ Chat.js
@@ -0,0 +1,42 @@
+/* Learning more about the API.
+Following the chat Stream tutorial. */
+
+import React from 'react';
+import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
+import { MessageList, MessageInput } from 'stream-chat-react';
+import { StreamChat } from 'stream-chat';
+
+import 'stream-chat-react/dist/css/index.css';
+
+const chatClient = new StreamChat('gx5a64bj4ptz');
+const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiZmFuY3ktc2lsZW5jZS02In0.qVFEOtoYY_bPqnuW2OAebhsa1rdHFs71WYApTtehqNQ';
+
+chatClient.setUser(
+  {
+       id: 'fancy-silence-6',
+       name: 'Fancy silence',
+       image: 'https://getstream.io/random_svg/?id=fancy-silence-6&name=Fancy+silence'
+  },
+  userToken,
+);
+
+const channel = chatClient.channel('messaging', 'godevs', {
+  // add as many custom fields as you'd like
+  image: 'https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png',
+  name: 'Talk about Go',
+});
+
+const Chat = () => (
+  <Chat client={chatClient} theme={'messaging light'}>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
+
+export default Chat;
