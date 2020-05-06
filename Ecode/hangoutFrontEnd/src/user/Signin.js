5d877158ce216eb4f10c2a52d3a14aa64d591b28
Antonio <sanchez.antonio.877@gmail.com>
2020-05-06 13:04:44 +0000

    Removing recaptcha error message

--- Ecode/hangoutFrontEnd/src/user/Signin.js
+++ Ecode/hangoutFrontEnd/src/user/Signin.js
@@ -88,19 +88,12 @@ class Signin extends Component {
 
         return (
             <div className="container">
-                <h2 className="mt-5 mb-5">SignIn</h2>
+                <h2 className="mt-5 mb-5">Sign In</h2>
                 <hr />
                 <SocialLogin />
 
                 <hr />
                 <br />
-
-                <div
-                    className="alert alert-danger"
-                    style={{ display: error ? "" : "none" }}
-                >
-                    {error}
-                </div>
 
                 {loading ? (
                     <div className="jumbotron text-center">
